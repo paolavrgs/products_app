859a83e4f9bfc8b980d5de7552a4f02e051a9797
Paola Vargas <vrgspaola68@gmail.com>
2018-10-07 00:02:40 -0400

    template :client added

--- app/controllers/app/front_controller.rb
+++ app/controllers/app/front_controller.rb
@@ -3,6 +3,27 @@ module App
 
     def index
     end
-
+     
+    private
+ 
+    def resource_name
+      :user
+    end
+    helper_method :resource_name
+   
+    def resource
+      @resource ||= User.new
+    end
+    helper_method :resource
+   
+    def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+    end
+    helper_method :devise_mapping
+   
+    def resource_class
+      User
+    end
+    helper_method :resource_class
   end
 end
